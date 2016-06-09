fae518b179858f49ec291833c64fce19eddeb680
Subbu64774 <subash.6471@gmail.com>
2016-06-09 00:52:29 -0700

    Create sumofnaturalns.java

---
+++ sumofnaturalns.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Subramanian Ganesan on 6/9/2016.
+ */
+public class sumofnaturalns {
+    public static void main(String[] args){
+    int sum=0;
+        for (int i=0;i<=1000;i++) {
+            sum+=i;
+
+        }
+        System.out.println(sum);
+    }
+}
