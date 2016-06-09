ec86b2243c7d2fbd308f962cc0c9e463af118104
Subbu64774 <subash.6471@gmail.com>
2016-06-08 23:53:56 -0700

    Create Positiveornegative.java

---
+++ Positiveornegative.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by Subramanian Ganesan on 6/8/2016.
+ */
+public class Positiveornegative {
+    public static void main(String[] args){
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the no to find positive or negative");
+        n=sc.nextInt();
+        if(n<0)
+            System.out.println(n+" is a negative no");
+        else
+            System.out.println(n+" is a positive no");
+
+    }
+}
