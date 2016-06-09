ea09a115745ba829cd4dd8c6df037ea91df15ab8
Subbu64774 <subash.6471@gmail.com>
2016-06-09 00:22:23 -0700

    Create printfivetimes.java

---
+++ printfivetimes.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Subramanian Ganesan on 6/9/2016.
+ */
+public class printfivetimes {
+    public static void main(String[] args){
+        String s="Hello World";
+        for(int i=0;i<=4;i++)
+            System.out.println(s);
+    }
+}
