1aa6829d88ddcaa4f1c1d1760b31173793d04549
Subbu64774 <subash.6471@gmail.com>
2016-06-08 23:54:53 -0700

    Create alphabettest.java

---
+++ alphabettest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Subramanian Ganesan on 6/8/2016.
+ */
+public class alphabettest {
+    public static void main(String args){
+        char c='s';
+
+        if(((c>='A')&&(c<='Z'))||((c>='a')&&(c<='z')))
+        System.out.println("This is a Alphabet");
+        else
+        System.out.println("Not a Alphbet");
+
+
+    }
+
+}
