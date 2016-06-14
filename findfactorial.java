f75d169141ea0a28da3e4cae5222d5d8ad310174
Subbu64774 <subash.6471@gmail.com>
2016-06-13 23:03:41 -0700

    Create findfactorial.java

---
+++ findfactorial.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Subramanian Ganesan on 6/14/2016.
+ */
+public class findfactorial
+{
+    public static void main(String[] ard1){
+        int no=3;
+        int fact=1;
+        for (int i=no;i>=1;i--)
+        {
+            fact=fact*i;
+
+
+        }
+        System.out.println(fact);
+    }
+
+}
